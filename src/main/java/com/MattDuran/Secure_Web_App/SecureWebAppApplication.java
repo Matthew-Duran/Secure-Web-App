e116db91c53d4a0b548a9d18fc3909f4fe88a851
Matthew Duran <Matthew.q.duran@gmail.com>
2025-09-04 14:30:49 -0400

    Updates

---
+++ src/main/java/com/MattDuran/Secure_Web_App/SecureWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.MattDuran.Secure_Web_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MattDuran/Secure_Web_App/SecureWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MattDuran.Secure_Web_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
